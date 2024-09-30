509a0d4b4d186bec7975da822d02fbb57cb844f1
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-10-01 00:21:06 +0500

    added chapter 1 & 2 alert and string for variables

---
+++ app.js
@@ -0,0 +1,66 @@
+//Alert chapter
+// 1. What does the alert function do in JavaScript?
+
+//Alert hamay browser pr popup message alert kr k dyta hai 
+// Alert ka syntax hai
+// alert();
+// alert ko lower case ma hi likhty hain
+
+
+
+// 2. Write the code to display an alert with the message "Welcome to my website!".
+// alert("Welcome to my website!.");
+
+// 3. What happens if you capitalize the alert keyword?
+// Agr capital letter ma likhygy ya first letter capital to error ayega.
+
+// 4 . Write the correct JavaScript syntax to display "Hello, world!" in an alert box.
+// alert("Hello, world!")
+
+// 5 . Write a JavaScript alert statement with proper formatting and explain each part of the
+// code.
+// alert ka pura syntax yeh hai
+// alert( ) :
+// alert keyword hai
+// ( ) isky andar jo hamnay show krwana hota ha osay likhty hai strings ma ya numbers ma
+// and last semicolon ;
+// Is k baghair bhi code run hojata ha lekin isay lgana zrori ha
+
+// chapter variable for string.
+ // 1 . Write code that declares a variable message, assigns the value "Hello, world!" to
+// it, and then displays an alert with that message.
+// var  message = "Hello, world!"
+// alert(message);
+
+// 2 . Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+// later statement. Display an alert showing the value of age.
+// var age = 25;
+// alert(age);
+
+//3 . Declare a variable named greeting, assign it the value "Good morning!", and then
+// display it in an alert. Afterwards, change the value of greeting to "Good evening!"
+// and display the new value in another alert.
+// Declare a variable named 'greeting' and assign it the value "Good morning!"
+// var greeting = "Good morning!";
+
+// // Display the initial value of greeting in an alert
+// alert(greeting);
+
+// // Change the value of greeting to "Good evening!"
+//  var greeting = "Good evening!";
+
+// // Display the new value of greeting in another alert
+// alert(greeting);
+
+// 4 . Create a variable called favoriteColor  and assign it the value "blue". Then display
+// an alert that says "My favorite color is blue" using the variable.                             
+// var favoriteColor = " Blue";
+// alert("My favorite color is" +  favoriteColor);
+
+// 5 . Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+// later statement. Display an alert showing the value of age.
+
+// var age = 25;
+// alert(age);
+
+
